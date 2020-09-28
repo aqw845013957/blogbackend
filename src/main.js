95e0b1ea326bcf1ee48f9df43f91517a75428f18
Tsubaha <845013957@qq.com>
2020-09-28 08:33:37 +0800

    后台管理

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+
+import './assets/normalize.less'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+Vue.use(ViewUI)
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
---
+++ src/views/article.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <Table stripe :columns="columns1" :data="data1">
+      <template slot="action">
+        <Button type="primary" size="small" style="margin-right: 5px"
+          >编辑</Button
+        >
+        <Button type="error" size="small">删除</Button>
+      </template>
+    </Table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      columns1: [
+        {
+          title: "Name",
+          key: "name",
+        },
+        {
+          title: "Age",
+          key: "age",
+        },
+        {
+          title: "Address",
+          key: "address",
+        },
+        {
+          title: "Action",
+          slot: "action",
+          width: 150,
+          align: "center",
+        },
+      ],
+      data1: [
+        {
+          name: "John Brown",
+          age: 18,
+          address: "New York No. 1 Lake Park",
+          date: "2016-10-03",
+        },
+        {
+          name: "Jim Green",
+          age: 24,
+          address: "London No. 1 Lake Park",
+          date: "2016-10-01",
+        },
+        {
+          name: "Joe Black",
+          age: 30,
+          address: "Sydney No. 1 Lake Park",
+          date: "2016-10-02",
+        },
+        {
+          name: "Jon Snow",
+          age: 26,
+          address: "Ottawa No. 2 Lake Park",
+          date: "2016-10-04",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
